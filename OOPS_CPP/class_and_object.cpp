1dca7b8f98c90519ed65ff071e0464a8486d3424
kartick-jangoo <kartick.kumarghosh@swift-nav.com>
2023-06-01 15:24:18 +0530

    updating codes

--- OOPS_CPP/class_and_object.cpp
+++ OOPS_CPP/class_and_object.cpp
@@ -1,7 +1,87 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <string>
 using namespace std;
 
-int main()
-{
-    cout<<"INTRODUCTION : OOPS IN C++"<<endl;
+enum FoodHabit {
+    herbivore,
+    carnivore,
+    omnivore
+};
+
+class animals {
+public:
+
+    // data members
+    int16_t *legs;
+    FoodHabit foodHabit;
+    string colour;
+    
+
+    // data method
+    void setLegs(int16_t x) {
+        *legs = x;
+    }
+    void setColour(const string& s);
+
+    // just a constructor
+    animals()
+    {
+        legs = new int16_t;
+        *legs = 0;
+        this->foodHabit = omnivore;
+        this->colour = "nil";
+    }
+
+
+    // parameterised constructor
+     animals(int16_t x, FoodHabit y, string z)
+    {
+        legs = new int16_t;
+        *legs = x;
+        this->foodHabit = y;
+        this->colour = z;
+    }
+
+    // copy constructor
+    animals(const animals &obj)
+    {
+        legs = new int16_t;
+
+        *legs = 4;
+        foodHabit = obj.foodHabit;
+        colour = obj.colour;
+    }
+};
+
+// Definition of data method outside the animals class
+void animals::setColour(const string& s) {
+    colour = s;
+}
+
+int16_t main() {
+    animals *cat1 = new animals(4,omnivore,"white");
+    animals *cat = cat1;
+   delete(cat1);
+    // animals cat; vs animals cat = new animals();
+    // cat.setLegs(4);
+    // cat.foodHabit = omnivore;
+    // cat.setColour("multi");
+
+    cout << *cat->legs << endl;
+
+    switch (cat->foodHabit) {
+        case herbivore:
+            cout << "Herbivore" << endl;
+            break;
+        case carnivore:
+            cout << "Carnivore" << endl;
+            break;
+        case omnivore:
+            cout << "Omnivore" << endl;
+            break;
+    }
+
+    cout << cat->colour << endl;
+
+    return 0;
 }

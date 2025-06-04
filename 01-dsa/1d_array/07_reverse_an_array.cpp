3b902ee1306b7a1ca4c7dc1c198ccc5976dd3cb2
gavinspet <kartykjdj@gmail.com>
2025-06-04 20:59:02 +0530

    changes in reverse array

--- 01-dsa/1d_array/07_reverse_an_array.cpp
+++ 01-dsa/1d_array/07_reverse_an_array.cpp
@@ -1,12 +1,28 @@
-#include "common.h"
 #include <bits/stdc++.h>
 using namespace std;
+
+void display(vector<int> nums)
+{
+  for(int i=0; i<nums.size();i++)
+  {
+    if(i != nums.size()-1)
+    {
+      cout<<nums[i]<<" ";
+    }
+    else
+    {
+      cout<<nums[i]<<endl; 
+    }
+  }
+}
 
 int main() {
   vector<int> nums;
   nums.assign({0, 1, 2, 3, 4, 5, 6});
   int n = 7;
   display(nums);
-  reverse(nums.begin() + 1, nums.begin() + 4);
+  int begin_index = 1;
+  int reverse_span = 4;
+  reverse(nums.begin() + begin_index, nums.begin() + reverse_span);
   display(nums);
 }
